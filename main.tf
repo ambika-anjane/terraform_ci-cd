3bf63e8eb5ed23c6f84ad7b8f2fe7cd5f71875af
ambika-anjane <96051172+ambika-anjane@users.noreply.github.com>
2025-01-23 12:47:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,9 @@ terraform {
 
 # S3 Bucket Integration for Snowflake
 resource "snowflake_storage_integration" "s3_integration" {
-  name                     = "S3_STORAGE_INTEGRATION"
+  name                     = "s3_int"
   storage_provider          = "S3"
   enabled                  = true
-  storage_aws_role_arn     = "arn:aws:iam::123456789012:role/SnowflakeRole" # Replace with actual role ARN
-  external_stage_location  = "s3://your-bucket-name/path/" # Replace with your actual S3 bucket and path
+  storage_aws_role_arn     = "arn:aws:iam::703671898489:role/json_test" # Replace with actual role ARN
   comment                  = "Integration with S3 for data loading and unloading"
 }
