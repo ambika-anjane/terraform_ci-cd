23886ffd5eaa6c1ff920c31d01c8ee145ef7cab9
ambika-anjane <96051172+ambika-anjane@users.noreply.github.com>
2025-01-23 11:50:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,10 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.87"
-    }
-  }
-}
+terraform { 
+  cloud { 
+    
+    organization = "Ambika_Terraform" 
 
-
-
-# S3 Bucket Integration for Snowflake
-resource "snowflake_storage_integration" "s3_integration" {
-  name                     = "S3_STORAGE_INTEGRATION"
-  storage_provider          = "S3"
-  enabled                  = true
-  storage_aws_role_arn     = "arn:aws:iam::123456789012:role/SnowflakeRole" # Replace with actual role ARN
-  external_stage_location  = "s3://your-bucket-name/path/" # Replace with your actual S3 bucket and path
-  comment                  = "Integration with S3 for data loading and unloading"
-}
-
-resource "snowflake_stage" "s3_stage" {
-  name                    = "S3_STAGE"
-  database                = snowflake_database.db.name
-  schema                  = "PUBLIC"
-  url                     = "s3://your-bucket-name/path/"  # Replace with your actual S3 bucket path
-  storage_integration     = snowflake_storage_integration.s3_integration.name
-  
- 
+    workspaces { 
+      name = "api_test" 
+    } 
+  } 
 }
